d6d0cbfc203d363c65aed3acd89ab7fd68ec408b
Jakob Olason <jakobolason@proton.me>
2025-08-23 08:56:12 +0200

    adds kitty conf with colors

--- scripts/alert-battery.sh
+++ scripts/alert-battery.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
 threshold=20  # threshold percentage to trigger alert
-critical_threshold = 5
+critical_threshold=10
 # Use `awk` to capture `acpi`'s percent capacity ($2) and status ($3) fields
 # and read their values into the `status` and `capacity` variables
 # uses grep because i somehow get two battery values?
-acpi -b | grep 'Battery 1' | awk -F'[,:%]' '{print $2, $3}' | {
+acpi -b | grep 'Battery 0' | awk -F'[,:%]' '{print $2, $3}' | {
   read -r status capacity
 
+  
+  if [ "${status}" = Discharging -a "${capacity}" -lt ${critical_threshold} ];
+  then
+    
+    echo -e "\aBatteriet er kritisk lavt\n Tilslut strømforsyning" | wofi -i --dmenu --width 350 --height 85 -j
+  fi
   # If battery is discharging with capacity below threshold
   if [ "${status}" = Discharging -a "${capacity}" -lt ${threshold} ];
   then
